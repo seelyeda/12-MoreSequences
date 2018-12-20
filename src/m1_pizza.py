79c5e30304e4383be59575a66f30447f9a865f77
Dave Seelye <seelyeda@rose-hulman.edu>
2018-12-20 12:32:00 -0500

    four

--- src/m1_pizza.py
+++ src/m1_pizza.py
@@ -231,7 +231,7 @@ def draw_points_on_circle(window, circle, number_of_points, color):
       :type color:            str
     """
     # -------------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     #
     # IMPLEMENTATION REQUIREMENT:
